9e46f6ee3afc379c3f01f34645375112ae832186
zill4 <justcrisp@outlook.com>
2024-09-26 10:41:02 -0500

    removed some files

--- config.py
+++ config.py
@@ -2,13 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.urandom(32)
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') + '?sslmode=require'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://localhost:5432/minimalistdevportfolio'  + '?sslmode=require' 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    # Email configuration
-    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.gmail.com')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT', 587))
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in ['true', 'on', '1']
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
