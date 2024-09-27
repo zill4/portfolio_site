8ede328ba3ce50182f1c3bf0a4c319e121827af0
zill4 <justcrisp@outlook.com>
2024-09-26 23:40:28 -0500

    update ignore

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.urandom(32)
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://localhost:5432/minimalistdevportfolio'  + '?sslmode=require' 
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- routes/__init__.py
+++
@@ -1,4 +0,0 @@
-from .main import main_bp
-from .blog import blog_bp
-from .projects import projects_bp
-from .admin import admin_bp
